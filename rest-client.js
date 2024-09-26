7f85047374c4a95d004b8f351c97b7a6e914998f
Opilane TTHK <opilane@tthk.ee>
2024-09-26 14:36:03 +0300

    Buttons dont work [app.#188337960]

--- rest-client.js
+++ rest-client.js
@@ -3,22 +3,46 @@ const vue = Vue.createApp({
         return {
             gameInModal: {name: null},
             games: [],
-            gameInfoModalInstance: null
+            newGame: {name: '', price: 0}
         }
     },
     async created() {
-        this.games = await (await fetch('http://localhost:8080/games')).json();
+        this.fetchGames();
     },
     methods: {
+        async fetchGames() {
+            this.games = await (await fetch('http://localhost:8080/games')).json();
+        },
         getGame: async function(id) {
             this.gameInModal = await (await fetch(`http://localhost:8080/games/${id}`)).json();
-            this.gameInfoModalInstance = new bootstrap.Modal(document.getElementById('gameInfoModal'), {});
-            this.gameInfoModalInstance.show();
+            let gameInfoModal = new bootstrap.Modal(document.getElementById('gameInfoModal'), {});
+            gameInfoModal.show();
         },
         closeModal() {
-            if (this.gameInfoModalInstance) {
-                this.gameInfoModalInstance.hide();
-            }
+            let modals = document.querySelectorAll('.modal.show');
+            modals.forEach(modal => bootstrap.Modal.getInstance(modal).hide());
+        },
+        showAddGameModal() {
+            let addGameModal = new bootstrap.Modal(document.getElementById('addGameModal'), {});
+            addGameModal.show();
+        },
+        async addGame() {
+            await fetch('http://localhost:8080/games', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(this.newGame)
+            });
+            this.newGame = {name: '', price: 0};
+            this.fetchGames();
+            this.closeModal();
+        },
+        async deleteGame(id) {
+            await fetch(`http://localhost:8080/games/${id}`, {
+                method: 'DELETE'
+            });
+            this.fetchGames();
         }
     }
-}).mount('#app')
+}).mount('#app');
